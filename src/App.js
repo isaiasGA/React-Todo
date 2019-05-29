19dbffdcc64d5aad896a60132b1231a282285968
IsaiasGA <isaiasgarciaariza@gmail.com>
2019-05-29 15:50:53 -0700

    Todo form has some data

--- src/App.js
+++ src/App.js
@@ -8,15 +8,37 @@ class App extends React.Component {
    this.state = {
     data: ExampleData,
     task: '',
-    id: 12 ,
+    id:Date.now(),
     completed: false,
    }
  }
+
+addTask = () => {
+  const newTask = {
+    task: '',
+    id: Date.now(),
+    completed: false,
+  }
+  
+  this.setState({
+    data: [...this.state.data, newTask],
+  })
+}
+
+/*
+ - addTask: A function used in order to call "this.state" so that the "data"
+    on state can be updated every time a new "task" is written as an input.
+ -seState: A function that we get to use via "React.Component"
+   -It is used in order to update state.
+ - Spread operator: It is used in order to copy the old data from an old array into a new array
+*/
+
   render() {
     return (
       <div className='main-content'>
         <h1>Todo List</h1>
-       < TodoList passedData={this.state.data} />
+       <TodoList passedData={this.state.data} />
+       <TodoForm newTaskData={this.addTask} />
       </div>
     );
   }
