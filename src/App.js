1a402667e15e37eb0a24676f3d4994546fefa0d1
IsaiasGA <isaiasgarciaariza@gmail.com>
2019-05-30 15:42:33 -0700

    Currently debugging

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import  {ExampleData} from './components/ExampleData';
 import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
  constructor () {
@@ -13,21 +14,30 @@ class App extends React.Component {
    }
  }
 
-addTask = () => {
+addTask = (event) => {
+  event.preventDefault();
   const newTask = {
-    task: '',
-    id: Date.now(),
-    completed: false,
+    task: this.state.task,
+    id: this.state.id,
+    completed: this.state.completed,
   }
   
   this.setState({
     data: [...this.state.data, newTask],
-  })
-}
+  });
+};
 
 /*
- - addTask: A function used in order to call "this.state" so that the "data"
+  * addTask: It creates a new task object each time its called.
+    - A function used in order to call "this.state" so that the "data"
     on state can be updated every time a new "task" is written as an input.
+    - each property on state is referencing the properties on our state in order to allow the user to ADD content when typing in our form.
+
+  * event.preventDefault(); It is used to prevent REFRESHING the webpage each time an user CLICKS on a buttons.
+
+  * Synthetic events: Events object that REACT creates off of an event that happened in the browser.
+
+
  -seState: A function that we get to use via "React.Component"
    -It is used in order to update state.
  - Spread operator: It is used in order to copy the old data from an old array into a new array
@@ -38,7 +48,7 @@ addTask = () => {
       <div className='main-content'>
         <h1>Todo List</h1>
        <TodoList passedData={this.state.data} />
-       <TodoForm newTaskData={this.addTask} />
+       <TodoForm addTask={this.addTask} />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+
+class TodoForm extends React.Component {
+    constructor(props) {
+        super (props);
+        this.state = {
+            list: ''
+        };
+    }
+
+    handleChanges = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+/*
+  * handleChanges: It is a useful function that is used when multiple INPUTS need to be handled.
+   - It identifies the correct input when a user types in in order to update it on state with the VALUE that was typed.
+
+  * Each time our user types inside our input, "todo", we will run "this.setState"
+     - we set up setState to list: "" property because it is controlling our input below
+  * HANDLECHANGES: a FUNCTION that will allow an user to TYPE in our form
+        - This function will update our state, list:'asdd', each time an user types input, "asdd"
+*/
+
+render() {
+    return (
+        <form onSubmit={props.addTask}>
+          <input placeholder='...todo'
+            type='text'
+            value={this.state.list}
+            name="list"
+            onChange={this.handleChanges}
+            />
+            <button>Add Todo</button>
+            <button>Clear Completed</button>
+        </form>
+    )
+}
+
+/*
+   * type, value, name, onChange: These are known as attributes
+   * value: An attribute that is used in order to reference the empty string in our class , list: ''.
+      - list is the only state property that we will refer to on the value attribute because it is the only one on our state.
+   * name: It is used in order to MATCH the correct PROPERTY on state that is CONTROLLING the INPUT in our form
+        - as for our form, we only have one property controlling our "list".
+        - having a name property will make out handleChanges property more dynamic.
+   * onChange: An event handler that will keeps track of all typing events happening in our input, '...todo'
+            - This event handler will refernce the function "handlechanges" above.
+             - handlechanges function allows the user to actually TYPE inside our form!!!
+*/
+
+} 
+
+export default TodoForm;
+
+
+
+
